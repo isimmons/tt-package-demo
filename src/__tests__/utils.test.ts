c1b0ed3817344f762bfb0490cf0a76eed9c8699b
Ian <isimmons33@gmail.com>
2024-10-03 23:44:47 -0400

    add subtract function

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,6 +1,10 @@
-import { add } from '../utils.js'
+import { add, subtract } from '../utils.js'
 import { expect, it } from 'vitest'
 
 it('should add two numbers', () => {
   expect(add(1, 2)).toBe(3)
+})
+
+it('should subtract two numbers', () => {
+  expect(subtract(1, 2)).toBe(-1)
 })
